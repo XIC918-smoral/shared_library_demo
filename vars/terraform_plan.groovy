e70f11f827521b693382761bb82373a49ba19374
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-02-17 17:16:14 +0530

    Update terraform_plan.groovy

--- vars/terraform_plan.groovy
+++ vars/terraform_plan.groovy
@@ -8,17 +8,17 @@ node {
         // **       in the global configuration.
         //mvnHome = tool 'M3'
     }
-    stage('tflint'){
-    echo "=========================Executing linting for Terraform files============================"
-    sh  'sudo docker run --rm -v $(pwd):/data -t ghcr.io/terraform-linters/tflint'
+//     stage('tflint'){
+//     echo "=========================Executing linting for Terraform files============================"
+//     sh  'sudo docker run --rm -v $(pwd):/data -t ghcr.io/terraform-linters/tflint'
    
-    }
+//     }
     
-    stage('checov'){
-    echo "=========================Executing checkov for Terraform files============================"
-    sh  'sudo docker pull bridgecrew/checkov'
-    sh 'sudo docker run --rm --volume $(pwd):/tf --workdir /tf bridgecrew/checkov --directory /tf'
-    }
+//     stage('checov'){
+//     echo "=========================Executing checkov for Terraform files============================"
+//     sh  'sudo docker pull bridgecrew/checkov'
+//     sh 'sudo docker run --rm --volume $(pwd):/tf --workdir /tf bridgecrew/checkov --directory /tf'
+//     }
     
     
     
@@ -45,23 +45,24 @@ node {
     }
     stage('Terraform plan') {
         sh 'terraform plan -out=plan.out'
+        sh 'terraform show -json plan.out | jq > plan.json'
     }
     
     
     
     
-    stage('infracost'){
-    echo "=========================Executing infracost============================"
-    sh  'terraform show -json plan.out > plan.json'
-    sh 'sudo docker run --rm   -e INFRACOST_API_KEY=ico-PdUKKbuT5d6Com9gKcccnWhWZXId6YWB  -v "$(pwd):/src" infracost/infracost breakdown --path  /src/plan.json --show-skipped'
+//     stage('infracost'){
+//     echo "=========================Executing infracost============================"
+//     sh  'terraform show -json plan.out > plan.json'
+//     sh 'sudo docker run --rm   -e INFRACOST_API_KEY=ico-PdUKKbuT5d6Com9gKcccnWhWZXId6YWB  -v "$(pwd):/src" infracost/infracost breakdown --path  /src/plan.json --show-skipped'
    
-    }
+//     }
     
-    stage('infracost difference'){
-    echo "=========================Executing infracost difference============================"
-    sh  'terraform show -json plan.out > plan.json'
-    sh 'sudo docker run --rm   -e INFRACOST_API_KEY=ico-PdUKKbuT5d6Com9gKcccnWhWZXId6YWB  -v "$(pwd):/src" infracost/infracost diff --path  /src/plan.json --show-skipped'
-   }
+//     stage('infracost difference'){
+//     echo "=========================Executing infracost difference============================"
+//     sh  'terraform show -json plan.out > plan.json'
+//     sh 'sudo docker run --rm   -e INFRACOST_API_KEY=ico-PdUKKbuT5d6Com9gKcccnWhWZXId6YWB  -v "$(pwd):/src" infracost/infracost diff --path  /src/plan.json --show-skipped'
+//    }
    
    
 }
