cfced93d5a93a626f694066c9fe739291d5f3447
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-02-17 18:47:11 +0530

    Update terraform_apply.groovy

--- vars/terraform_apply.groovy
+++ vars/terraform_apply.groovy
@@ -1,14 +1,14 @@
 def call() {
 node {
-    stage('git checkout') { // for display purposes
-        // Get some code from a GitHub repository
-        git branch: 'main', url: 'https://github.com/XIC918-smoral/ec2.git'
+//     stage('git checkout') { // for display purposes
+//         // Get some code from a GitHub repository
+//         git branch: 'main', url: 'https://github.com/XIC918-smoral/ec2.git'
         
-    }
+//     }
     
-    stage('Terraform init') {
-       sh 'terraform init'
-    }
+//     stage('Terraform init') {
+//        sh 'terraform init'
+//     }
      stage('Approval') {
             // no agent, so executors are not used up when waiting for approvals
                 script {
