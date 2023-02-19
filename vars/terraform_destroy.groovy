e24e9f03c6e192d84f157eeca1f4af9df8063493
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-02-19 23:07:50 +0530

    Update terraform_destroy.groovy

--- vars/terraform_destroy.groovy
+++ vars/terraform_destroy.groovy
@@ -1,3 +1,4 @@
+def call() {
 node {
     stage('git checkout') { // for display purposes
         // Get some code from a GitHub repository
@@ -8,15 +9,16 @@ node {
     stage('Terraform init') {
        sh 'terraform init'
     }
-//      stage('Approval') {
-//             // no agent, so executors are not used up when waiting for approvals
-//                 script {
-//                     def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'rkivisto,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
-//                     sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
-//                 }
-//      }
+     stage('Approval') {
+            // no agent, so executors are not used up when waiting for approvals
+                script {
+                    def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'rkivisto,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
+                    sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
+                }
+     }
     
     stage('Terraform destory') {
        sh 'terraform destroy --auto-approve'
     }
+}
 }
