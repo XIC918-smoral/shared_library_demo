f90b11f8391549bcdbc9e0e77f093097b4eb6816
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-02-19 16:42:05 +0530

    Update terraform_apply.groovy

--- vars/terraform_apply.groovy
+++ vars/terraform_apply.groovy
@@ -11,7 +11,7 @@ node {
 //     }
         
         stage('copy'){
-        copyArtifacts fingerprintArtifacts: true, projectName: 'terraform', selector: upstream(fallbackToLastSuccessful: true)
+        copyArtifacts filter: 'plan.json', fingerprintArtifacts: true, projectName: 'terraform', selector: upstream(fallbackToLastSuccessful: true)
         }
 //      stage('Approval') {
 //             // no agent, so executors are not used up when waiting for approvals
