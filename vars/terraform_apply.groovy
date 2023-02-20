4dbe3be06439924dbc87d22b4255fbca32835639
XIC918-smoral <101389596+XIC918-smoral@users.noreply.github.com>
2023-02-20 09:18:32 +0530

    Update terraform_apply.groovy

--- vars/terraform_apply.groovy
+++ vars/terraform_apply.groovy
@@ -12,13 +12,13 @@ node {
 //     stage('Terraform init') {
 //        sh 'terraform init'
 //     }
-        stage('cd'){
-                sh 'cd terraform-${BUILD_NUMBER}'
-        }
+//         stage('cd'){
+//                 sh 'cd terraform-${BUILD_NUMBER}'
+//         }
         
-        stage('copy'){
-       copyArtifacts filter: '**', fingerprintArtifacts: true, includeBuildNumberInTargetPath: true, projectName: 'terraform', selector: workspace()
-        }
+//         stage('copy'){
+//        copyArtifacts filter: '**', fingerprintArtifacts: true, includeBuildNumberInTargetPath: true, projectName: 'terraform', selector: workspace()
+//         }
 //      stage('Approval') {
 //             // no agent, so executors are not used up when waiting for approvals
 //                 script {
